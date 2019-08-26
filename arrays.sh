ccc123ea3f40ead50ff6fe77ddf7eb1191cc1e6d
mgtrjemilio <54515791+mgtrjemilio@users.noreply.github.com>
2019-08-25 22:10:28 -0500

    arreglos

---
+++ arrays.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Muestra el uso de arrays
+# Podemos crear arrays de una dimensión
+arreglo[0]="Plutarco"
+arreglo[1]="Hackrearth"
+arreglo[2]="NavarradotNET"
+arreglo[3]="Riberlug"
+# Otra forma de definirlos
+comida=("tacos" "hotdogs" "pizza" "burger" "sushi" "sopa" )
+numeros=(15 23 45 42 23 1337 23 666 69)
+echo ${arreglo[0]} es una parte de un arreglo simple, ${comida[1]} es una comida.
+
