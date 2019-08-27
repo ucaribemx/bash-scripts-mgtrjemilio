76dd93a7dcacd9348788234e9d7cd744208e02dd
mgtrjemilio <54515791+mgtrjemilio@users.noreply.github.com>
2019-08-26 21:40:24 -0500

    librerias

---
+++ libreria.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# No es que existan las librerías pero se puede simular
+# algo similar
+
+# Esta es la forma de importar funciones
+source Funct.sh
+muestrapantalla 69 123 "epa"
+sumador 1337 3389 && echo "OK" || echo "Ocurrió un error"
+echo "Resultado: ${RESULTADO} $!"
